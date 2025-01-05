e32058cde27889c2fe49cc16d55f215b2a1c3d36
andrenunes <andremarcelonunes@gmail.com>
2025-01-05 09:51:33 -0300

    removendo arquivo do git

--- backendeldery/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://elderydb:teste123@localhost/elderly_care"
-    MONGO_URI: str = "mongodb://localhost:27017"
-    MONGO_DB: str = "elderly_care"
-
-settings = Settings()
